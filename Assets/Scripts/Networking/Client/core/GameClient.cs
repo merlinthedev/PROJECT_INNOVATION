19c80c59a75dc1d5d31d36a3777ea281b0233a0d
Merlijn van Nieuwenhoven <merlijnvan11@gmail.com>
2023-03-29 16:56:15 +0200

    fsm & refactor
    removed fsm and refactored objects that depended on the fsm. other broken scenes because of "pray for this merge fellas" merge. NEEDS TESTING

---
+++ Assets/Scripts/Networking/Client/core/GameClient.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using shared;
+
+public class GameClient : MonoBehaviour {
+
+    private TcpMessageChannel tcpMessageChannel;
+
+    private static GameClient instance;
+
+    private void Awake() {
+        if(instance != null) {
+            Debug.LogError("There is already an exisiting GameClient present. Aborting instantiation.");
+            Destroy(this);
+            return;
+        }
+
+        tcpMessageChannel = new TcpMessageChannel();
+    }
+
+    private void Update() {
+        if (tcpMessageChannel.Connected) receiveAndProcessMessages();
+        
+    }
+
+    private void receiveAndProcessMessages() {
+        if(!tcpMessageChannel.Connected) {
+            Debug.LogWarning("Trying to receive messages but we are no longer connected...");
+            return;
+        }
+
+        while (tcpMessageChannel.HasMessage() && gameObject.activeSelf) {
+            ASerializable message = tcpMessageChannel.ReceiveMessage();
+            handleNetworkMessage(message);
+        }
+    }
+
+    private void handleNetworkMessage(ASerializable message) {
+        // handle messages
+        Debug.Log("Received a message of type " + message.GetType() + ", but we have no way of handling it yet...");
+    }
+
+    private void OnApplicationQuit() {
+        tcpMessageChannel.Close();
+    }
+}
