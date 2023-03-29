d312b9b7d2a9cd874e5e873c1bfd4ad2aacbeec6
marsmaantje[laptop] <marsmaantje@planet.nl>
2023-03-29 15:10:58 +0200

    Improved cart controller (hopefully)

--- Assets/Scripts/ShoppingCartController.cs
+++ Assets/Scripts/ShoppingCartController.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShoppingCartController : MonoBehaviour {
-    Rigidbody rb;
-
+    
     private Vector3 smoothAcceleration = Vector3.zero;
     [SerializeField] private float smootSpeed = 5f;
     // Start is called before the first frame update
 
-    [SerializeField] private float rotationSpeed = 90f;
-    [SerializeField] private float rotationForce = 1f;
-    [SerializeField] private float maxRotationForce = 1f;
-
     [SerializeField] private AnimationCurve inputCurve;
 
     [SerializeField] private ButtonPressed gasButton;
+    [SerializeField][Range(0, 1)] private float gasSpeed = 1f;
     [SerializeField] private ButtonPressed brakeButton;
-    [SerializeField] private float forwardSpeed = 1f;
-    [SerializeField] private float backwardSpeed = 1f;
-    [SerializeField] private float maxForce = 1f;
+    [SerializeField][Range(0, 1)] private float brakeSpeed = 0.5f;
+
+    [SerializeField] private ShoppingCartMovement movement;
 
     void Start() {
-        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -31,22 +26,22 @@ public class ShoppingCartController : MonoBehaviour {
         Vector3 acceleration = -Input.acceleration;
         smoothAcceleration = Vector3.Lerp(smoothAcceleration, acceleration, smootSpeed * Time.deltaTime);
 
-        float targetRotationSpeed = inputCurve.Evaluate(Mathf.Abs(smoothAcceleration.x)) * -Mathf.Sign(smoothAcceleration.x) * rotationSpeed;
+        float rotationInput = inputCurve.Evaluate(Mathf.Abs(smoothAcceleration.x)) * -Mathf.Sign(smoothAcceleration.x);
 
+        movement.DoView(new Vector2(rotationInput, 0));
 
         Vector3 targetVelocity = Vector3.zero;
+        float movementInput = 0;
         
         if (gasButton.isPressed) {
-            targetVelocity += transform.forward * forwardSpeed;
+            movementInput += gasSpeed;
         }
 
         if (brakeButton.isPressed) {
-            targetVelocity += -transform.forward * backwardSpeed;
-            targetRotationSpeed *= Mathf.Sign(Vector3.Dot(rb.velocity, transform.forward));
+            movementInput -= brakeSpeed;
         }
 
-        rb.angularVelocity = new Vector3(0, targetRotationSpeed, 0);
-        rb.velocity = Vector3.Lerp(rb.velocity, targetVelocity, Time.deltaTime * maxForce);
+        movement.DoMove(new Vector2(0, movementInput));
         
     }
 }
