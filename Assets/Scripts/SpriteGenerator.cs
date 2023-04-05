ba2d3cb8a81fcb8338af54d9d861e586cf706db2
marsmaantje[laptop] <marsmaantje@planet.nl>
2023-04-05 15:25:12 +0200

    Sprite renderer

--- Assets/Scripts/SpriteGenerator.cs
+++ Assets/Scripts/SpriteGenerator.cs
@@ -8,19 +8,30 @@ public class SpriteGenerator : MonoBehaviour {
     public string spriteName;
     
     public void RenderSprite() {
-        //save what the camera sees to a sprite asset in the root of our assets folder
-        Texture2D texture = new Texture2D(imageSize.x, imageSize.y, TextureFormat.RGB24, false);
+        RenderTexture renderTexture = new RenderTexture(imageSize.x, imageSize.y, 24);
+        camera.targetTexture = renderTexture;
+        Texture2D texture = new Texture2D(imageSize.x, imageSize.y, TextureFormat.RGBA32, false);
         Rect rect = new Rect(0, 0, imageSize.x, imageSize.y);
+
+        camera.Render();
+        var activeTexture = RenderTexture.active;
+        RenderTexture.active = renderTexture;
         texture.ReadPixels(rect, 0, 0);
         texture.Apply();
-        byte[] bytes = texture.EncodeToPNG();
-        string path = Application.dataPath + "/" + spriteName + ".png";
-        System.IO.File.WriteAllBytes(path, bytes);
-        
-#if UNITY_EDITOR
-        AssetDatabase.Refresh();
-#endif
-        
-        Destroy(texture);
+
+        camera.targetTexture = null;
+        RenderTexture.active = activeTexture;
+        Destroy(renderTexture);
+
+        var sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+
+        var path = EditorUtility.SaveFilePanelInProject("Save Sprite", spriteName, "png", "Save Sprite", "Assets");
+        if (path.Length != 0) {
+            var bytes = texture.EncodeToPNG();
+            System.IO.File.WriteAllBytes(path, bytes);
+            AssetDatabase.Refresh();
+        }
+
+        DestroyImmediate(texture);
     }
 }
