6255342a43e42639753ce533600fa867692ab927
Merlijn van Nieuwenhoven <merlijnvan11@gmail.com>
2023-04-10 17:57:15 +0200

    mesh colliders

--- Assets/Scripts/Storezone/Storezone.cs
+++ Assets/Scripts/Storezone/Storezone.cs
@@ -6,7 +6,7 @@ public class Storezone : MonoBehaviour {
     [Tooltip("The discount that the player gets when they enter the storezone, should be a number between 0 and 1")]
     [SerializeField] private float storeDiscount = 0.1f;
     [SerializeField] private float storeDiscountChangeInterval = 60f;
-    [SerializeField] private BoxCollider storezoneCollider;
+    [SerializeField] private Collider storezoneCollider;
 
     [SerializeField] private float minDiscount = 0.1f;
     [SerializeField] private float maxDiscount = 0.5f;
@@ -42,6 +42,9 @@ public class Storezone : MonoBehaviour {
         }
     }
 
+    public void OnTriggerEnter(Collider other) {
+        Debug.Log("Entered storezone");
+    }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
